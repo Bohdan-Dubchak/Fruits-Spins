442e21d0d61dd1000b63592a2d65beeb8e695e8a
Bogdan <dubchakbohdan63@gmail.com>
2026-05-04 21:27:25 +0200

    feat: update checWin (Win render)

--- src/scene/GameScene.ts
+++ src/scene/GameScene.ts
@@ -2,9 +2,12 @@ import {Assets, Container, Sprite} from "pixi.js";
 import {ReelContainer} from "../reels/ReelsContainer.ts";
 import {SpinButton} from "../ui/spinButton.ts";
 import {GAME_CONFIG} from "../config/constants.ts";
+import {payLines, payTable} from "../config/paylines.ts";
+import {WinCalculator} from "../game/engine/WinCalculator.ts";
 
 export class GameScene extends Container {
     private reelsContainer!: ReelContainer;
+    private payTable:  Record<string, Record<number, number>> = payTable;
     // private uiContainer = new Container();
 
     constructor() {
@@ -53,69 +56,30 @@ export class GameScene extends Container {
         this.addChild(spinButton);
     }
 
-    // Опис відповідностей
-    private payTable:  Record<string, Record<number, number>> = {
-        bell:   { 3: 7, 4: 25, 5: 100 },
-        cherry: { 3: 5, 4: 20, 5: 40 },
-        grapes: { 3: 25, 4: 50, 5: 100 },
-        lemon:  { 3: 5, 4: 20, 5: 40 },
-        orange: { 3: 5, 4: 25, 5: 50 },
-        plum:   { 3: 15, 4: 25, 5: 50 },
-        seven:  { 3: 200, 4: 500, 5: 2500 },
-    };
-
     // Виграш
     private checkWin(): void {
 
         const reels = this.reelsContainer.getReels();
-        const matrix = reels.map((reel) => reel.getVisibleSymbolsSprites());
 
-        const payLines = [
-            [0, 0, 0, 0, 0],
-            [1, 1, 1, 1, 1],
-            [2, 2, 2, 2, 2],
-        ];
+        const matrix = reels.map((reel) => {
 
-        let totalWin = 0;
-
-        payLines.forEach((line, lineIndex) => {
-
-            const symbols = line.map((row, reelIndex) => {
-                const sprite = matrix[reelIndex][row];
+            return reel.getVisibleSymbolsSprites().map(sprite => {
                 return (sprite as any).symbolId;
             });
-
-            console.log(`Лінія ${lineIndex + 1}:`, symbols);
-
-            const firstSymbol = symbols[0];
-
-            // рахуємо скільки однакових підряд зліва
-            let count = 1;
-
-            for (let i = 1; i < symbols.length; i++) {
-                if (symbols[i] === firstSymbol) {
-                    count++;
-                } else {
-                    break;
-                }
-            }
-
-            // мінімум 3 для виграшу
-            if (count >= 3) {
-
-                const win = this.payTable[firstSymbol]?.[count] || 0;
-
-                totalWin += win;
-
-                console.log(
-                    `🎉 Виграш!`,
-                    `Символ: ${firstSymbol}`,
-                    `Кількість: ${count}`,
-                    `Сума: ${win}`
-                );
-            }
         });
 
-        console.log("💰 Загальний виграш:", totalWin);
+        const result = WinCalculator.calculate(matrix, payLines, this.payTable);
+
+        result.wins.forEach(win => {
+
+            console.log(
+                `🎉 Line ${win.lineIndex + 1}`,
+                `Symbol: ${win.symbol}`,
+                `Count: ${win.count}`,
+                `Win: ${win.amount}`
+            );
+        });
+
+        console.log("💰 TOTAL:", result.totalWin);
     }
 }
