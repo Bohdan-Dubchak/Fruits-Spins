c04a93cff79db8f96170caf7feca5790d9820862
Bogdan <dubchakbohdan63@gmail.com>
2026-05-11 19:47:46 +0200

    feat: update pat table

--- src/config/paylines.ts
+++ src/config/paylines.ts
@@ -1,10 +1,10 @@
 // Опис відповідностей
 export const payTable:  Record<string, Record<number, number>> = {
     bell:   { 3: 7, 4: 25, 5: 100 },
-    cherry: { 3: 5, 4: 20, 5: 40 },
+    cherry: { 3: 8, 4: 20, 5: 40 },
     grapes: { 3: 25, 4: 50, 5: 100 },
-    lemon:  { 3: 5, 4: 20, 5: 40 },
-    orange: { 3: 5, 4: 25, 5: 50 },
+    lemon:  { 3: 8, 4: 20, 5: 40 },
+    orange: { 3: 9, 4: 25, 5: 50 },
     plum:   { 3: 15, 4: 25, 5: 50 },
     seven:  { 3: 200, 4: 500, 5: 2500 },
 };
