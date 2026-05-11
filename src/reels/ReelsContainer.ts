4c39bf839974822b4eef584e667ddec34ca36b3a
Bogdan <dubchakbohdan63@gmail.com>
2026-05-11 19:46:34 +0200

    feat: create RNG

--- src/reels/ReelsContainer.ts
+++ src/reels/ReelsContainer.ts
@@ -1,20 +1,23 @@
 import {Container} from "pixi.js";
 import {Reel} from "./Reels.ts";
+import {RNG} from "./RNG.ts";
 
 export class ReelContainer extends Container {
     private reels: Reel[] = [];
     private REEL_GAP: number = 150;
     private reelCount: number;
+    private rng: RNG;
 
-    constructor(reelCount: number) {
+    constructor(reelCount: number, rng: RNG) {
         super();
         this.reelCount = reelCount;
+        this.rng = rng;
         this.createReel()
     }
 
     private createReel(): void {
         for (let i = 0; i < this.reelCount; i++) {
-            const reel = new Reel();
+            const reel = new Reel(this.rng);
             reel.init();
             reel.x = i * this.REEL_GAP;
             reel.y = 50;
@@ -33,7 +36,7 @@ export class ReelContainer extends Container {
 
         // Зупиняємо по черзі з затримкою
         for (let i = 0; i < this.reels.length; i++) {
-            await new Promise(resolve => setTimeout(resolve, 1500 + i));
+            await new Promise(resolve => setTimeout(resolve, 1500 + i * 300));
             this.reels[i].stop();
         }
 
