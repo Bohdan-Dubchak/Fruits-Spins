7c783b1acdce3b02487cb59768626cfd3d0e1052
Bogdan <dubchakbohdan63@gmail.com>
2026-05-17 01:22:08 +0200

    feat: update win animation

--- src/game/win/WinHandler.ts
+++ src/game/win/WinHandler.ts
@@ -1,19 +1,25 @@
+import {Sprite} from "pixi.js";
 import { WalletManager } from "../wallet/WalletManager.ts";
 import { WinText } from "../../ui/display/WinText.ts";
 import { HUD } from "../../ui/display/HUD.ts";
 import { WinCalculator } from "../calculator/WinCalculator.ts";
 import { payLines, payTable } from "../../constants/paylines.ts";
 import { WinTextAnimation } from "../../animations/WinAnimations.ts";
+import {ReelContainer} from "../../reels/ReelsContainer.ts";
+import {animationSymbols} from "../../animations/ReelSymbolAnimation.ts";
+
 
 export class WinHandler {
     private wallet: WalletManager;
     private winText: WinText;
     private hud: HUD;
+    private reelsContainer: ReelContainer
 
-    constructor(wallet: WalletManager, winText: WinText, hud: HUD) {
+    constructor(wallet: WalletManager, winText: WinText, hud: HUD, reelsContainer: ReelContainer) {
         this.wallet = wallet;
         this.winText = winText;
         this.hud = hud;
+        this.reelsContainer = reelsContainer;
     }
 
     handleWin(matrix: string[][], bet: number): void {
@@ -26,6 +32,27 @@ export class WinHandler {
 
         if (result.totalWin > 0) {
             this.logWins(result.wins);
+
+            // Анімація виграшних символів
+            const winningSprites: Sprite[] = [];
+
+            result.wins.forEach(win => {
+                const line = payLines[win.lineIndex];
+
+                // Беремо тільки стільки символів, скільки виграло
+                for (let i = win.startPosition; i < win.startPosition + win.count; i++) {
+                    const row = line[i];
+                    const sprite = this.reelsContainer.getSymbolSprite(i, row);
+                    if (sprite && !winningSprites.includes(sprite)) {
+                        winningSprites.push(sprite);
+                    }
+                }
+            });
+
+            // Викликаємо анімацію
+            if (winningSprites.length > 0) {
+                animationSymbols(winningSprites);
+            }
 
             this.wallet.addWin(result.totalWin);
             this.hud.updateBalance(this.wallet.getBalance());
