473979d00489c41da42c1fce7cbc7c06610839fe
Bogdan <dubchakbohdan63@gmail.com>
2026-05-18 21:46:36 +0200

    feat: update TS

--- src/scene/MenuScene.ts
+++ src/scene/MenuScene.ts
@@ -3,19 +3,20 @@ import {GAME_CONFIG} from "../config/game.ts";
 import {PlayButton} from "../ui/button/playButton.ts";
 
 export class MenuScene extends Container {
+    private playBtn: PlayButton;
+
     constructor(startCallback: () => void) {
         super();
 
         const texture = Assets.get('MenuFon');
         const bg = new Sprite(texture);
-
         bg.setSize(GAME_CONFIG.WIDTH, GAME_CONFIG.HEIGHT);
         bg.position.set(0, 0);
 
-        const playBtn = new PlayButton(startCallback);
-        playBtn.setSize(270, 87)
-        playBtn.position.set(533, 183);
+        this.playBtn = new PlayButton(startCallback);
+        this.playBtn.setSize(270, 87);
+        this.playBtn.position.set(533, 183);
 
-        this.addChild(bg, playBtn);
+        this.addChild(bg, this.playBtn);
     }
 }
