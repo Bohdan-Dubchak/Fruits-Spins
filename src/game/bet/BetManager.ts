773a65725c95e22b0688b394848bc9d248e97cd5
Bogdan <dubchakbohdan63@gmail.com>
2026-05-23 22:37:57 +0200

    feat: new functions set spinning

--- src/game/bet/BetManager.ts
+++ src/game/bet/BetManager.ts
@@ -5,18 +5,26 @@ export class BetManager {
 
     private readonly INTERVAL_DELAY: number = 190;
 
+    private isSpinning: boolean = false;
+
     private wallet: WalletManager;
     private onBetChange: (bet: number) => void;
 
-    constructor(
-        wallet: WalletManager,
-        onBetChange: (bet: number) => void
-    ) {
+    constructor(wallet: WalletManager, onBetChange: (bet: number) => void) {
         this.wallet = wallet;
         this.onBetChange = onBetChange;
     }
 
+    setSpinning(spinning: boolean): void {
+        this.isSpinning = spinning;
+        if (spinning) {
+            this.stop();
+        }
+    }
+
     startIncreasing(): void {
+        if (this.isSpinning) return;
+
         this.stop();
 
         this.interval = setInterval(() => {
@@ -26,6 +34,8 @@ export class BetManager {
     }
 
     startDecreasing(): void {
+        if (this.isSpinning) return;
+
         this.stop();
 
         this.interval = setInterval(() => {
@@ -35,11 +45,13 @@ export class BetManager {
     }
 
     increaseBetOnce(): void {
+        if (this.isSpinning) return;
         this.wallet.increaseBet();
         this.onBetChange(this.wallet.getBet());
     }
 
     decreaseBetOnce(): void {
+        if (this.isSpinning) return;
         this.wallet.decreaseBet();
         this.onBetChange(this.wallet.getBet());
     }
