f07bc13be1caf4a65cb334e25dad64ecdf494189
Bogdan <dubchakbohdan63@gmail.com>
2026-05-27 17:13:24 +0200

    feat: add sound

--- src/managers/InfoPanelManager.ts
+++ src/managers/InfoPanelManager.ts
@@ -1,6 +1,7 @@
 import {Container} from "pixi.js";
 import {InfoPanel} from "../ui/display/InfoPanel.ts";
 import {WalletManager} from "../game/wallet/WalletManager.ts";
+import {SoundManager} from "../audio/SoundManager.ts";
 
 export class InfoPanelManager {
     private currentPanel: InfoPanel | null = null;
@@ -9,12 +10,14 @@ export class InfoPanelManager {
     private readonly container: Container;
     private readonly gameWidth: number;
     private readonly gameHeight: number;
+    private soundManager: SoundManager;
 
-    constructor(wallet: WalletManager, container: Container, gameWidth: number, gameHeight: number) {
+    constructor(wallet: WalletManager, container: Container, gameWidth: number, gameHeight: number, soundManager: SoundManager) {
         this.wallet = wallet;
         this.container = container;
         this.gameWidth = gameWidth;
         this.gameHeight = gameHeight;
+        this.soundManager = soundManager;
     }
 
     public show(): void {
@@ -28,7 +31,8 @@ export class InfoPanelManager {
             this.lastWinAmount,
             currentBalance,
             this.gameWidth,
-            this.gameHeight
+            this.gameHeight,
+            this.soundManager
         );
 
         this.currentPanel.onClose(() => {
