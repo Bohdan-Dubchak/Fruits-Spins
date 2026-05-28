536ae597bde2bea0e8ea3e696e500b8ee94d8813
Bogdan <dubchakbohdan63@gmail.com>
2026-05-28 22:47:45 +0200

    feat: add isSoundMuted

--- src/ui/UIFactory.ts
+++ src/ui/UIFactory.ts
@@ -35,7 +35,7 @@ export class UIFactory {
             () => betManager.stop()
         );
 
-        const soundBtn = new SoundButton(() => this.soundManager.toggleSound());
+        const soundBtn = new SoundButton(() => this.soundManager.toggleSound(), this.soundManager.isSoundMuted());
         const infoBtn = new InfoBtn(onInfo, this.soundManager);
 
         return {
